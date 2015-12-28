8e7163d2b8a058d3c7fe332b9ce6150f6783179b
Oliver Gugenheim <ogugenheim@drw.com>
2015-12-28 15:04:48 -0600

    Add a global test runner

---
+++ TestMain.hs
@@ -0,0 +1,18 @@
+module Main ( main ) where
+
+import TestGem
+import Gem
+import Test.QuickCheck
+
+main :: IO ()
+main = mapM_ quickCheck [prop_adds_right,
+                           test_blank_to_row,
+                           test_blank_to_col,
+                           test_blank_to_right,
+                           test_n_to_last_column,
+                           test_blank_to_left,
+                           test_n_to_place,
+                           test_solve_top_row,
+                           test_finish_top_row 
+                        ]
+ 
---
+++ clean.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+rm Gem.o
+rm Gem.hi
+rm TestGem.o
+rm TestGem.hi
+rm TestMain.o
+rm TestMain.hi
+rm TestMain
