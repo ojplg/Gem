48264664241f04bcb7d6525b887909ea7f542c3c
Oliver Gugenheim <lees@ripco.com>
2016-02-17 13:39:48 -0600

    Add test to suite

--- TestMain.hs
+++ TestMain.hs
@@ -18,6 +18,7 @@ main = mapM_ quickCheck [prop_adds_right,
                            test_finish_top_row,
                            test_finish_top_two_rows,
                            test_finish_top_rows,
-                           test_solve_front_next_to_last_row
+                           test_solve_front_next_to_last_row,
+                           test_solve_next_to_last_row
                         ]
  
