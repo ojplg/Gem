a4b02554300c65feb64c8618f4d1bb8267bab746
Oliver Gugenheim <lees@ripco.com>
2016-02-21 10:54:58 -0600

    Improve clean function

--- clean.sh
+++ clean.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-rm -f *.o
-rm -f *.hi
+function remove_haskell_object_files() {
+  rm -f *.o
+  rm -f *.hi
+}
+
+remove_haskell_object_files
 rm -f TestMain
+cd Gem
+remove_haskell_object_files
